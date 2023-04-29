6e4bfe6e5e126fe38bb814989d09cf15d835ef9f
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-04-29 17:56:54 -0500

    adding configs and express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log(`it works`);
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
+import bodyParser from "body-parser";
+import express from "express";
+
+import { config } from "./config";
+
+function main() {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use("/**", (_req, res) => {
+    res.status(200).send("<h1>it works!</h1>");
+  });
+
+  const { port } = config.server;
+
+  app.listen(port, () => {
+    console.log(`[APP] - Starting application on port ${port}`);
+    console.log(`http://localhost:${port}`);
+  });
+}
+
+main();
