9a2a2d2b158479a2bb9a271992b5f5c5c7d4bd1a
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-01 00:39:56 -0500

    update dependencies with Movie instances

--- src/infrastructure/dependencies.ts
+++ src/infrastructure/dependencies.ts
@@ -1,12 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
+import { MovieCritic } from "../application/MovieCritic";
+import { MovieCrud } from "../application/MovieCrud";
 import { UserCrud } from "../application/UserCrud";
+import { MovieController } from "./MovieController";
+import { PrismaMovieRepository } from "./PrismaMovieRepository";
 import { PrismaUserRepository } from "./PrismaUserRepository";
 import { UserController } from "./UserController";
 
 const prismaClient = new PrismaClient();
 
 const userRepo = new PrismaUserRepository(prismaClient);
+const movieRepo = new PrismaMovieRepository(prismaClient);
+
 const userCrud = new UserCrud(userRepo);
+const movieCrud = new MovieCrud(movieRepo);
+const movieCritic = new MovieCritic(movieRepo);
 
 export const userController = new UserController(userCrud);
+export const movieController = new MovieController(movieCrud, movieCritic);
