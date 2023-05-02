bfb89689036b8a5ffb78271d9fc05305687b7fdf
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-02 08:16:25 -0500

    Installed swagger

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,24 @@ dotEnvConfig();
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import swaggerJSDoc, { Options } from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 import { config } from "./config";
 import { apiRouter } from "./infrastructure/routes";
 
 function main() {
   const app = express();
+  const swaggerSpec: Options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Movie admin",
+        version: "1.0.0"
+      }
+    },
+    apis: ["./infrastructure/routes/*.ts"]
+  };
 
   app.use(morgan("tiny"));
   app.use(cors());
@@ -17,7 +29,13 @@ function main() {
   app.get("/health", (_req, res) => {
     res.status(200).send("ok");
   });
+
   app.use("/api", apiRouter);
+  app.use(
+    "/api-doc",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerJSDoc(swaggerSpec))
+  );
 
   const { port } = config.server;
 
